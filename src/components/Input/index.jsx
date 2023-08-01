a865959613009a9726aa0e23df044ec2baebcd8a
Yurcik45 <yurcik45mail@gmail.com>
2023-08-01 18:27:20 +0300

    update atoms styles

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,9 +1,14 @@
 import s from './index.module.css'
 
-export const Input = ({ placeholder, value, onChange, disabled }) =>
+export const Input = ({ placeholder, name, value, onChange, centered, width, disabled }) =>
 <input
   className={ s.Input }
+  style={{
+    width: width ?? "100%",
+    textAlign: centered ? "center" : "initial"
+  }}
   placeholder={ placeholder }
+  name={ name }
   value={ value }
   onChange={ onChange }
   disabled={ disabled }
