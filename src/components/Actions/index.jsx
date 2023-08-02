8a80b844e72d2041d07adeb16eb203ce628621e1
yurcik45 <yurcik45mail@gmail.com>
2023-08-02 14:21:47 +0000

    add Actions component

---
+++ src/components/Actions/index.jsx
@@ -0,0 +1,13 @@
+import { add_expensess_icon, add_benefit_icon } from '../../assets/common'
+import { Button } from '../Button'
+import s from './index.module.css'
+
+export const Actions = ({ type, onAdd }) =>
+<div className={ s.container }>
+  <Button
+    title={ "ad" + " " + type }
+    onClick={ () => onAdd(type) }
+    icon={ type === "expensess" ? add_expensess_icon : add_benefit_icon }
+    border_bottom
+  />
+ </div>
