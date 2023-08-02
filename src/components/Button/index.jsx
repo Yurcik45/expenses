aaad38b47c8c5b0e0849856af2f531551e00935d
yurcik45 <yurcik45mail@gmail.com>
2023-08-02 14:25:23 +0000

    update Button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,16 +1,19 @@
 import s from './index.module.css'
 
-export const Button = ({ title, color, bg_color, icon, reverse, onClick, disabled }) =>
+export const Button = ({ title, color, bg_color, border_bottom, icon, reverse, onClick, disabled }) =>
 <button
   className={ s.Button }
-  styles={{
+  style={{
     color: color ?? "black",
     backgroundColor: bg_color ?? "white",
-    flexDirection: reverse ? "row-reverse" : "row"
+    flexDirection: reverse ? "row-reverse" : "row",
+    justifyContent: icon ? "space-between" : "center",
+    borderRadius: border_bottom ? 0 : 15,
+    ...( border_bottom ? { borderBottom: "2px solid black" } : { border: "2px solid black" } )
   }}
   onClick={ onClick }
   disabled={ disabled }
 >
-  <div>{ title }</div>
-  { icon && <div>{ icon }</div> }
+  <div className={ s.title }>{ title }</div>
+  { icon && <div className={ s.icon }>{ icon }</div> }
 </button>
