35d80eb7eed6eab63b46022ab5d2c3ba15538edc
yurcik45 <yurcik45mail@gmail.com>
2023-08-03 09:43:02 +0000

    add Select component

---
+++ src/components/Select/index.jsx
@@ -0,0 +1,48 @@
+import { useState, useRef } from 'react'
+import { v4 as uuid } from 'uuid'
+import ClickAwayListener from 'react-click-away-listener'
+import { select_arrow_icon } from '../../assets/common'
+import s from './index.module.css'
+
+export const Select = ({ label, items, selected_item, onClick }) =>
+{
+  const [active, set_active] = useState(false)
+  const selectRef = useRef(null);
+
+  const current = selectRef?.current !== null ? selectRef.current : {};
+
+  const close = () => set_active(false);
+
+  const select = item  =>
+  {
+    setValue(item)
+    close()
+  }
+
+  return (
+    <ClickAwayListener onClickAway={ close }>
+      <div
+        className={ `${ s.container } ${ active ? s.container_active : "" }}` }
+        ref={ selectRef }
+        onClick={ () => set_active(!active) }
+      >
+        { label && <div className={ s.label }>{ label }</div> }
+        <div className={ s.main_item }>{ selected_item ?? "categories" }</div>
+        <div className={ active ? s.icon : s.icon_reverse }>{ select_arrow_icon }</div>
+        { active && <div
+          className={ s.select_container }
+          style={{
+            top: current["offsetTop"] + current["offsetHeight"] - 2,
+            left: current["offsetLeft"],
+            width: current["offsetWidth"] - 2,
+          }}
+        >
+          { items.map(item => <div key={ uuid() } className={ s.item } onClick={ () => onClick(item) } >
+              { item.name }
+            </div>)
+          }
+        </div>}
+      </div>
+    </ClickAwayListener>
+  )
+}
