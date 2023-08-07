16b903dd4607fce22a8cad7c12fa341f656338ef
Yurcik45 <yurcik45mail@gmail.com>
2023-08-07 10:40:26 +0300

    separate logic to prevent update after each change on input, upgrade to work with mobx

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,39 +1,62 @@
+import { useState } from 'react'
 import { observer } from 'mobx-react'
 import { Input } from '../Input'
 import { Button } from '../Button'
 import { edit_icon, save_icon, trash_icon } from '../../assets/common'
 import s from './index.module.css'
 
-export const Item = observer(({ item, index, is_editing, onWantChange, onChange, onSave, onDelete }) =>
-  <div className={ s.Item }>
-    <div className={ s.id }>{ index + 1 }</div>
-    <div className={ s.group }>
-      <Input
-        placeholder={ item.name }
-        name="name"
-        value={ is_editing  ? item.name : null }
-        onChange={ event => onChange(item.id, event) }
-        width={ 120 }
-        disabled={ !is_editing }
-      />
-      <Input
-        placeholder={ item.sum }
-        name="sum"
-        value={ is_editing  ? item.sum : null }
-        onChange={ event => onChange(item.id, event) }
-        width={ 50 }
-        centered
-        disabled={ !is_editing }
-      />
-    </div>
-    <div className={ s.group }>
-      <div
-        className={ s.edit_icon }
-        onClick={ () => (is_editing ? onSave : onWantChange)(item) }
-      >
-        { is_editing ? save_icon : edit_icon }
+export const Item = observer(({ item, index, onSave, onDelete }) =>
+{
+  const [editing_item, set_editing_item] = useState(null)
+
+  const change = (id, event) =>
+  {
+    const { name, value } = event.target
+    set_editing_item({ ...editing_item, [name]: name === "sum" ? +value : value })
+  }
+
+  const want_change = () =>
+  {
+    set_editing_item({ ...item })
+  }
+
+  const save = () =>
+  {
+    onSave(editing_item)
+    set_editing_item(null)
+  }
+  
+  return (
+    <div className={ s.Item }>
+      <div className={ s.id }>{ index + 1 }</div>
+      <div className={ s.group }>
+        <Input
+          placeholder={ item.name }
+          name="name"
+          value={ editing_item === null ? item.name : editing_item.name }
+          onChange={ event => change(item.id, event) }
+          width={ 120 }
+          disabled={ editing_item === null }
+        />
+        <Input
+          placeholder={ item.sum }
+          name="sum"
+          value={ editing_item === null ? item.sum : editing_item.sum }
+          onChange={ event => change(item.id, event) }
+          width={ 50 }
+          centered
+          disabled={ editing_item === null }
+        />
       </div>
-      <div className={ s.trash_icon } onClick={ () => onDelete(item.id) } >{ trash_icon }</div>
+      <div className={ s.group }>
+        <div
+          className={ s.edit_icon }
+          onClick={ editing_item === null ? want_change : save }
+        >
+          { editing_item === null ? edit_icon : save_icon }
+        </div>
+        <div className={ s.trash_icon } onClick={ () => onDelete(item.id) } >{ trash_icon }</div>
+      </div>
     </div>
-  </div>
-)
+  )
+})
--- src/components/ItemsList/index.jsx
+++ src/components/ItemsList/index.jsx
@@ -6,32 +6,16 @@ import s from './index.module.css'
 
 export const ItemsList = inject('expensesStore')(observer(({ expensesStore, list_name }) =>
 {
-  const [editing_item, set_editing_item] = useState(null)
-
-  const change = (id, event) =>
-  {
-    const { name, value } = event.target
-    set_editing_item({ ...editing_item, [name]: name === "sum" ? +value : value })
-  }
-
-  const want_change = item =>
-  {
-    set_editing_item({ ...item })
-  }
-
   const save = item =>
   {
-    // .. change item on mobx store
-    expensesStore[ list_name === "expenses" ? "editExpense" : "editBenefit" ](editing_item)
-    set_editing_item(null)
-    // .. make request to update item globally
+    console.log("save changed item: ", item)
+    expensesStore.editItem(list_name, item)
   }
 
   const delete_el = id =>
   {
-    // .. delete item from mobx store
-    expensesStore[ list_name === "expenses" ? "deleteExpense" : "deleteBenefit" ](id)
-    // .. make request to delete item globally
+    console.log("delete item id: ", id)
+    expensesStore.deleteItem(list_name, id)
   }
 
   const is_editing = id => editing_item?.id === id
@@ -41,11 +25,8 @@ export const ItemsList = inject('expensesStore')(observer(({ expensesStore, list
     { expensesStore[list_name].map((item, index) =>
       <Item
         key={ uuid() }
-        item={ is_editing(item.id) ? editing_item : item }
+        item={ item }
         index={ index }
-        is_editing={ is_editing(item.id) }
-        onWantChange={ want_change }
-        onChange={ change }
         onSave={ save }
         onDelete={ delete_el }
       />
