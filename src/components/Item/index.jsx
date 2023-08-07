1b5c936756c14e7a62824db81996fb114e26725b
Yurcik45 <yurcik45mail@gmail.com>
2023-08-07 15:22:43 +0300

    separate logic to Modal Component

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,62 +1,24 @@
-import { useState } from 'react'
-import { observer } from 'mobx-react'
 import { Input } from '../Input'
-import { Button } from '../Button'
-import { edit_icon, save_icon, trash_icon } from '../../assets/common'
+import { more_icon } from '../../assets/common'
 import s from './index.module.css'
 
-export const Item = observer(({ item, index, onSave, onDelete }) =>
-{
-  const [editing_item, set_editing_item] = useState(null)
-
-  const change = (id, event) =>
-  {
-    const { name, value } = event.target
-    set_editing_item({ ...editing_item, [name]: name === "sum" ? +value : value })
-  }
-
-  const want_change = () =>
-  {
-    set_editing_item({ ...item })
-  }
-
-  const save = () =>
-  {
-    onSave(editing_item)
-    set_editing_item(null)
-  }
-  
-  return (
-    <div className={ s.Item }>
-      <div className={ s.id }>{ index + 1 }</div>
-      <div className={ s.group }>
-        <Input
-          placeholder={ item.name }
-          name="name"
-          value={ editing_item === null ? item.name : editing_item.name }
-          onChange={ event => change(item.id, event) }
-          width={ 120 }
-          disabled={ editing_item === null }
-        />
-        <Input
-          placeholder={ item.sum }
-          name="sum"
-          value={ editing_item === null ? item.sum : editing_item.sum }
-          onChange={ event => change(item.id, event) }
-          width={ 50 }
-          centered
-          disabled={ editing_item === null }
-        />
-      </div>
-      <div className={ s.group }>
-        <div
-          className={ s.edit_icon }
-          onClick={ editing_item === null ? want_change : save }
-        >
-          { editing_item === null ? edit_icon : save_icon }
-        </div>
-        <div className={ s.trash_icon } onClick={ () => onDelete(item.id) } >{ trash_icon }</div>
-      </div>
-    </div>
-  )
-})
+export const Item = ({ item, index, onClick }) =>
+<div className={ s.Item } onClick={ onClick }>
+  <div className={ s.id }>{ index + 1 }</div>
+  <div className={ s.group }>
+    <Input
+      placeholder={ item.name }
+      name="name"
+      width={ 120 }
+      disabled
+    />
+    <Input
+      placeholder={ item.sum }
+      name="sum"
+      width={ 120 }
+      disabled
+    />
+    <div className={ s.category }>{ item.category }</div>
+  </div>
+  <div className={ s.more }>{ more_icon }</div>
+</div>
--- src/components/ItemsList/index.jsx
+++ src/components/ItemsList/index.jsx
@@ -2,18 +2,25 @@ import { useState } from 'react'
 import { observer, inject } from 'mobx-react'
 import { v4 as uuid } from 'uuid'
 import { Item } from '../Item'
+import { ItemModal } from '../ItemModal'
 import s from './index.module.css'
 
 export const ItemsList = inject('expensesStore')(observer(({ expensesStore, list_name }) =>
 {
+  const [modal_item, set_modal_item] = useState(null)
+
+  const reset_modal = () => set_modal_item(null)
+
   const save = item =>
   {
     expensesStore.editItem(list_name, item)
+    reset_modal()
   }
 
   const delete_el = id =>
   {
     expensesStore.deleteItem(list_name, id)
+    reset_modal()
   }
 
   return (
@@ -23,10 +30,17 @@ export const ItemsList = inject('expensesStore')(observer(({ expensesStore, list
         key={ uuid() }
         item={ item }
         index={ index }
-        onSave={ save }
-        onDelete={ delete_el }
+        onClick={ () => set_modal_item({ ...item }) }
       />
     ) }
+    { modal_item &&
+        <ItemModal
+          item={ modal_item }
+          onClose={ reset_modal }
+          onSave={ save }
+          onDelete={ delete_el }
+        />
+    }
    </div>
   )
 }))
